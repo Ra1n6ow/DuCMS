31f09f66ff96ac19395c446aac78d5e567fcc8d9
jfDu <jeffduuu@gmail.com>
2023-12-28 23:59:27 +0800

    添加 main 子组件

---
+++ src/components/mainHeader/mainHeader.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="main-header">
+    <div class="menu-icon" @click="handleMenuBtnClick">
+      <!-- 子组件三元运算，这里如果没有全局导入icon组件会不生效 -->
+      <component :is="isFold ? 'icon-menu-unfold' : 'icon-menu-fold'" size="26px" />
+      <!-- <component :is="'icon-menu-fold'" size="26px" />
+      <icon-menu-unfold size="26px" />
+      <icon-menu-fold size="26px" /> -->
+    </div>
+    <div class="content">
+      <div class="breadcrumb">面包屑</div>
+      <div>个人信息</div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const isFold = ref(false)
+
+// 定义emit 事件
+const emit = defineEmits(['foldChange'])
+
+function handleMenuBtnClick() {
+  isFold.value = !isFold.value
+
+  // 将foldChange事件传递给父组件
+  emit('foldChange', isFold.value)
+}
+</script>
+
+<style lang="less" scoped>
+.main-header {
+  display: flex;
+  align-items: center;
+  flex: 1;
+  height: 100%;
+
+  .menu-icon {
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+  }
+
+  .content {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex: 1;
+    padding: 0 18px;
+  }
+}
+</style>
---
+++ src/components/mainMenu/mainMenu.vue
@@ -0,0 +1,124 @@
+<template>
+  <div class="main-menu">
+    <!-- logo -->
+    <div class="logo">
+      <img class="img" src="@/assets/img/logo.svg" />
+      <!-- 折叠状态才显示 -->
+      <h2 v-if="!isFold" class="title">后台管理系统</h2>
+    </div>
+    <!-- 导航 -->
+    <div class="navdemo">
+      <a-menu :default-open-keys="['38']" :default-selected-keys="['39']" :collapsed="isFold">
+        <template v-for="item in userMenus" :key="item.id">
+          <!-- arco 组件在v-for条件下设置key会报错，需要在 arco 组件加个判断 -->
+          <a-sub-menu v-if="item.id" :key="item.id + ''">
+            <!-- 通过服务器获取 icon 字符串,这里没测试数据，所以写死, 这里可以使用动态组件,
+            <template #icon>
+              <component :is="'icon-desktop'" /> 将字符串换成对应的变量就行了
+            </template>
+           -->
+            <template #icon><icon-desktop /></template>
+            <template #title>{{ item.name }}</template>
+            <template v-for="subitem in item.children" :key="subitem.id">
+              <a-menu-item v-if="subitem.id" :key="subitem.id + ''">{{ subitem.name }}</a-menu-item>
+            </template>
+          </a-sub-menu>
+        </template>
+      </a-menu>
+      <!-- <a-menu>
+        <a-sub-menu key="0">
+          <template #icon><icon-desktop /></template>
+          <template #title>Navigation 1</template>
+          <a-menu-item key="0_0">Menu 1</a-menu-item>
+          <a-menu-item key="0_1">Menu 2</a-menu-item>
+        </a-sub-menu>
+        <a-sub-menu key="1">
+          <template #icon><icon-bug></icon-bug></template>
+          <template #title>Navigation 2</template>
+          <a-menu-item key="1_0">Menu 1</a-menu-item>
+          <a-menu-item key="1_1">Menu 2</a-menu-item>
+          <a-menu-item key="1_2">Menu 3</a-menu-item>
+        </a-sub-menu>
+        <a-sub-menu key="2">
+          <template #icon><icon-bulb></icon-bulb></template>
+          <template #title>Navigation 3</template>
+          <a-menu-item key="2_0">Menu 1</a-menu-item>
+          <a-menu-item key="2_1">Menu 2</a-menu-item>
+        </a-sub-menu>
+      </a-menu> -->
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import useLoginStore from '@/store/login'
+
+defineProps({
+  isFold: {
+    type: Boolean,
+    default: false
+  }
+})
+
+const loginStore = useLoginStore()
+const userMenus = loginStore.userMenus
+</script>
+
+<style lang="less" scoped>
+.main-menu {
+  height: 100%;
+  background-color: #001529;
+}
+
+.logo {
+  display: flex;
+  height: 28px;
+  padding: 12px 10px 8px 0px;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  overflow: hidden;
+
+  .img {
+    height: 100%;
+    margin: 0 10px;
+  }
+
+  .title {
+    font-size: 16px;
+    font-weight: 700;
+    color: white;
+    white-space: nowrap;
+  }
+}
+
+.arco-menu {
+  background-color: #001529;
+  :deep(.arco-menu-inline-header.arco-menu-selected:hover) {
+    background-color: #001529;
+  }
+  :deep(.arco-menu-inline-header.arco-menu-selected) {
+    color: #fff;
+  }
+  :deep(.arco-menu-inline-header) {
+    color: #b7bdc3;
+    background-color: #001529;
+  }
+  .arco-menu-item.arco-menu-selected {
+    color: #fff;
+    background-color: #0a60bd;
+  }
+  .arco-menu-item {
+    color: #b7bdc3;
+    background-color: #001529;
+    // background-color: #0c2135;
+  }
+  :deep(.arco-menu-selected .arco-icon) {
+    color: #fff;
+  }
+  :deep(.arco-menu-pop) {
+    color: #b7bdc3;
+    background-color: #001529;
+  }
+}
+</style>
